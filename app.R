baea371e4ee176f053d9d7a614a9d7513f88d382
Alexis <alexfootbal8496@gmail.com>
2018-07-06 16:40:11 -0400

    Test

---
+++ app.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+# Define UI ----anel
+ui <- fluidPage(
+  
+  navbarPage("Pool 2018-2019",
+             tabPanel("Acceuil",
+                      
+                      HTML( paste(h4("Yolo")))
+                      
+             ),
+             tabPanel("Stats Poolers"
+                      
+                      
+             ),
+             tabPanel("Échange"
+                      
+             ),
+             tabPanel("Création d'équipe"
+                      
+                      
+             )               
+  )
+  
+  
+  
+)
+
+# Define server logic ----
+server <- function(input, output, session) {
+  
+  
+  
+}
+# Run the app ----
+shinyApp(ui = ui, server = server)
+
