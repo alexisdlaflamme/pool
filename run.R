0e538a464b54387bb5119ac7e65bfb6be48aa04f
Alexis <alexfootbal8496@gmail.com>
2018-07-06 16:49:03 -0400

    Test DT

--- run.R
+++ run.R
@@ -8,16 +8,4 @@ shiny::runApp(
   port = as.numeric(port)
 )
 
-my_packages = c("DT")
-
-###########################################################
-
-install_if_missing = function(p) {
-  if (p %in% rownames(installed.packages()) == FALSE) {
-    install.packages(p, dependencies = TRUE)
-  }
-  else {
-    cat(paste("Skipping already installed package:", p, "\n"))
-  }
-}
-invisible(sapply(my_packages, install_if_missing))
+install.packages("DT")
