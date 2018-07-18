41c1acb834b935cb1731109af9bd11bd96c2bc4b
Alexis <alexfootbal8496@gmail.com>
2018-07-17 23:30:29 -0400

    Test dataBase connection 2

--- Programme/BaseDonnee.R
+++ Programme/BaseDonnee.R
@@ -9,26 +9,26 @@ library(tidyverse)
 
 # use the heroku command-line app
 # we do this as the creds change & it avoids disclosure
-#config <- run("heroku config:get DATABASE_URL -a testpool")
+config <- run("heroku config:get DATABASE_URL -a testpool")
 
 # ^^ gets us a URL, we need the parts
-#pg <- httr::parse_url(config$stdout)
+pg <- httr::parse_url(config$stdout)
 
 # use the parts from ^^
-#dbConnect(RPostgres::Postgres(),
-#          dbname = trimws(pg$path),
-#          host = pg$hostname,
-#          port = pg$port,
-#          user = pg$username,
-#          password = pg$password,
-#          sslmode = "require"
-#)-> db_con
+dbConnect(RPostgres::Postgres(),
+          dbname = trimws(pg$path),
+          host = pg$hostname,
+          port = pg$port,
+          user = pg$username,
+          password = pg$password,
+          sslmode = "require"
+)-> db_con
 
-dbcon<-dbConnect(RPostgres::Postgres(),dbname = 'd2tn74iejrtdue', 
-                 host = 'ec2-50-16-231-2.compute-1.amazonaws.com', 
-                 port = 5432, 
-                 user = 'rkvaizleemdacw',
-                 password = '7efca54493c95afc32fd648ac7bbdf0f2c4f6ba935e2b3fd7745d6bf545d2972') 
+#dbcon<-dbConnect(RPostgres::Postgres(),dbname = 'd2tn74iejrtdue', 
+#                 host = 'ec2-50-16-231-2.compute-1.amazonaws.com', 
+#                 port = 5432, 
+#                 user = 'rkvaizleemdacw',
+#                 password = '7efca54493c95afc32fd648ac7bbdf0f2c4f6ba935e2b3fd7745d6bf545d2972') 
 
 
 # hook it up to dbplyr
