78e78b8ea7647a8ccf7afc21f1ba8cdd9e4b36da
Alexis <alexfootbal8496@gmail.com>
2018-07-17 23:12:28 -0400

    Test base donnée

--- Programme/BaseDonnee.R
+++ Programme/BaseDonnee.R
@@ -9,28 +9,34 @@ library(tidyverse)
 
 # use the heroku command-line app
 # we do this as the creds change & it avoids disclosure
-config <- run("heroku", c("config:get", "DATABASE_URL", "-a", "poolhockey"))
+#config <- run("heroku config:get DATABASE_URL -a testpool")
 
 # ^^ gets us a URL, we need the parts
-pg <- httr::parse_url(config$stdout)
+#pg <- httr::parse_url(config$stdout)
 
 # use the parts from ^^
-dbConnect(dbDriver("PostgreSQL"),
-          dbname = trimws(pg$path),
-          host = pg$hostname,
-          port = pg$port,
-          user = pg$username,
-          password = pg$password,
-          sslmode = "require"
-) -> db_con
+#dbConnect(RPostgres::Postgres(),
+#          dbname = trimws(pg$path),
+#          host = pg$hostname,
+#          port = pg$port,
+#          user = pg$username,
+#          password = pg$password,
+#          sslmode = "require"
+#)-> db_con
+
+dbcon<-dbConnect(RPostgres::Postgres(),dbname = 'd2tn74iejrtdue', 
+                 host = 'ec2-50-16-231-2.compute-1.amazonaws.com', 
+                 port = 5432, 
+                 user = 'rkvaizleemdacw',
+                 password = '7efca54493c95afc32fd648ac7bbdf0f2c4f6ba935e2b3fd7745d6bf545d2972') 
+
 
 # hook it up to dbplyr
-db <- src_dbi(db_con)
+#db <- src_dbi(dbcon)
 
 # boom
-db
+#db
 ## src:  PqConnection
 ## tbls:
 
-xdf <- copy_to(db, iris, name="iris", overwrite = TRUE)
-
+#dbListTables(dbcon)
