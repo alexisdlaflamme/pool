9e8049ab4a628677291986d238e7ceae5481b688
Alexis <alexfootbal8496@gmail.com>
2018-07-19 19:07:56 -0400

    test database connection

--- Programme/BaseDonnee.R
+++ Programme/BaseDonnee.R
@@ -11,7 +11,7 @@ postgress.connect <- function() {
   #attempt connection
   if (Sys.getenv("DATABASE_URL") != "") {
     url <- httr::parse_url(Sys.getenv("DATABASE_URL"))
-    pool <- dbPool(drv
+    pool <- dbConnect(drv
                    , host     = url$hostname
                    , port     = url$port
                    , user     = url$user
@@ -21,10 +21,24 @@ postgress.connect <- function() {
     print("Heroku application detected connecting to Postgres")
     
   } else {
-    # You would setup a localhost  db connection to your personal computer here!
+    ##Ajuster##
+    
+    pool <- dbConnect(RPostgres::Postgres()
+                   , host     = "ec2-50-16-231-2.compute-1.amazonaws.com"
+                   , port     = 5432
+                   , user     = "rkvaizleemdacw"
+                   , password = "7efca54493c95afc32fd648ac7bbdf0f2c4f6ba935e2b3fd7745d6bf545d2972"
+                   , dbname   = "d2tn74iejrtdue"
+                   
+    )
+    print("Heroku application detected connecting to Postgres")
   }
   
   return(pool)
 }
 
+
+
 pg <- postgress.connect()
+
+
