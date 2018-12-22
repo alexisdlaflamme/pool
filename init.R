0ad9679d5f51c994d2cded485c352c530b00d40f
TBaill <thomas.baillargeon@gmail.com>
2018-12-22 11:46:57 -0500

    Ajout des library pour les graphiques.

--- init.R
+++ init.R
@@ -1,6 +1,6 @@
 
 my_packages = c("DT", "jsonlite", "dplyr", "readxl", "shinyWidgets", "processx", "RPostgreSQL", "RPostgres", "httr", 
-                "XML", "RCurl", "rlist","dbplyr", "devtools", "shinythemes")
+                "XML", "RCurl", "rlist","dbplyr", "devtools", "shinythemes", "ggplot2","plotly")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
