84c547e0eaa44ef9c3253d6e4545f4053d416bd0
Alexis <alexfootbal8496@gmail.com>
2019-01-17 00:24:05 -0500

    Oups

--- Programme/ui.R
+++ Programme/ui.R
@@ -245,23 +245,24 @@ ui <-navbarPage("Pool 2018-2019", theme = shinytheme("cerulean"),
                                   
                           ),
                           tags$head(
-                           HTML(
-                             "
-                            <script>
-                            var socket_timeout_interval
-                            var n = 0
-                            $(document).on('shiny:connected', function(event) {
-                            socket_timeout_interval = setInterval(function(){
-                            Shiny.onInputChange('count', n++)
-                            }, 15000)
-                            });
-                            $(document).on('shiny:disconnected', function(event) {
-                            clearInterval(socket_timeout_interval)
-                            });
-                            </script>
-                            "
-                           )
-                         ),
-                         textOutput("keepAlive")
+                            HTML(
+                              "
+                              <script>
+                              var socket_timeout_interval
+                              var n = 0
+                              $(document).on('shiny:connected', function(event) {
+                              socket_timeout_interval = setInterval(function(){
+                              Shiny.onInputChange('count', n++)
+                              }, 15000)
+                              });
+                              $(document).on('shiny:disconnected', function(event) {
+                              clearInterval(socket_timeout_interval)
+                              });
+                              </script>
+                              "
+                            )
+                            ),
+                          textOutput("keepAlive")
                 )
+      )
 )
