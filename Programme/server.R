79b52c832d3b4743ce8e6a8bdc23839c1d24ab8a
Alexis <alexfootbal8496@gmail.com>
2019-10-08 23:04:21 -0400

    Graph Acceuil

--- Programme/server.R
+++ Programme/server.R
@@ -116,8 +116,7 @@ server <- function(input, output, session) {
       infoPoolers<- dbReadTable(con,"infoPoolers")
       name <- colnames(evoPtsTotal[2:length(evoPtsTotal)])
       name <- as.Date(as.character(gsub('X', "", gsub(".","-",name, fixed = T))))
-      name<- c(as.Date("2018-10-03"), name)
-      pts<- unname(cbind(rep(0,6), evoPtsTotal[,2:length(evoPtsTotal)]))
+      pts<- unname(cbind(rep(0,5), evoPtsTotal[,2:length(evoPtsTotal)]))
       p<- plot_ly() %>% layout(title = "Evolution Pts totaux")
       for (i in 1:6){
         p<- add_trace(p, x = name, y = pts[i,], type = "scatter", mode = 'lines',color = I(infoPoolers$Couleur[i]), name = infoPoolers$Nom[i] )
